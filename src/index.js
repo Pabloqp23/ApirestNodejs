829e223e980c8ee91deaa563693b69687c48b25e
Pabloqp23 <112915699+Pabloqp23@users.noreply.github.com>
2022-11-03 14:55:58 -0600

    Second commit

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 require("dotenv").config();
 const userRoutes = require("./routes/user");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 9000;
 // middleware
 app.use(express.json());
-app.use('/api', userRoutes);
+app.use(cors());
+app.use("/api", userRoutes);
+
 
 // routes
-app.get('/', (req,res)=>{
-    res.send('Welcome to my Api')
+app.get("/", (req, res) => {
+  res.send("Welcome to my Api");
 });
 
 // mongoDB connection
-mongoose.connect(process.env.MONGODB_URI)
-.then(() => console.log('Connected to MongoDB Atlas'))
-.catch((error) => console.error(error));
-app.listen(port, () => console.log('server lisenig on port', port));
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch((error) => console.error(error));
+app.listen(port, () => console.log("server lisenig on port", port));
